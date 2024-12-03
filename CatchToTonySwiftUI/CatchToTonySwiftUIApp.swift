6aacd73bab7c583cf0efbc28b37a6b946014f828
Ömer Apaydın <omerapaydin90@gmail.com>
2024-12-03 19:28:53 +0300

    Initial Commit

---
+++ CatchToTonySwiftUI/CatchToTonySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchToTonySwiftUIApp.swift
+//  CatchToTonySwiftUI
+//
+//  Created by Ömer on 3.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CatchToTonySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchToTonySwiftUITests/CatchToTonySwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  CatchToTonySwiftUITests.swift
+//  CatchToTonySwiftUITests
+//
+//  Created by Ömer on 3.12.2024.
+//
+
+import Testing
+@testable import CatchToTonySwiftUI
+
+struct CatchToTonySwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
